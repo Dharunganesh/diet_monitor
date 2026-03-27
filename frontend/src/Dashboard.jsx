8592ffb266e6861caa0b4cdacdc28a2c93ac87d6
Dharunganesh <147619802+Dharunganesh@users.noreply.github.com>
2026-03-27 12:54:02 +0530

    Refactor submit button handling in Dashboard component

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -1,10 +1,8 @@
 function Dashboard(){
     const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-    let submit_button=document.getElementById("submit_button");
-    submit_button.onclick= async function(){
-        let text_content=document.getElementById("text_content");
-        text_content.textContent="Your response have been submitted";
-    }
+    const handleSubmit = () => {
+        setMessage("Your response has been submitted");
+    };
     return(
         <div>
             <h1>Welcome to Dashboard</h1>
@@ -12,8 +10,8 @@ function Dashboard(){
             <label>Enter your food record:</label>
             <input type="file" id="photo"></input>
             <br></br>
-            <button type="submit" id="submit_button">Submit</button>
-            <h2 id="text_content"></h2>
+            <button onClick={handleSubmit}>Submit</button>
+            <h2>{message}</h2>
         </div>
     )
 }
